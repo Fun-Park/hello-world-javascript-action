3ad2f750f7911e8ac119b68854be7baa25f8bc38
Surahman Sada <surahman.sada@gmail.com>
2023-10-25 14:55:59 +1000

    updated index.js

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const github = require('@actions/github');
 try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
+  const who = core.getInput('who');
   console.log(`Hello ${nameToGreet}!`);
+  console.log(`Hello ${who}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
+  core.setOutput("when", time);
  // Get the JSON webhook payload for the event that triggered the workflow
  const payload = JSON.stringify(github.context.payload, undefined, 2)
  console.log(`The event payload: ${payload}`);
- 
+
 }catch (error) {
   core.setFailed(error.message);
 }
